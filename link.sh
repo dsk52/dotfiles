7d64c58984278ef10f0afa3f9b678257aa505e9b
Daisuke KONISHI <skydai1151@gmail.com>
2025-08-09 15:29:34 +0900

    feat: claudecode回りの設定ファイルを追加

--- link.sh
+++ link.sh
@@ -6,13 +6,22 @@ if [ ! -d $HOME/.config ]; then
   mkdir $HOME/.config
 fi
 
+# fish
 rm -rf $HOME/.config/fish
 ln -sfnv $DIRNAME/.config/fish $HOME/.config/fish
 
-ln -sfnv $DIRNAME/.editorconfig $HOME/.editorconfig
-
+# git
 ln -sfnv $DIRNAME/.gitconfig $HOME/.gitconfig
 ln -sfnv $DIRNAME/.gitignore $HOME/.gitignore
 ln -sfnv $DIRNAME/.gitexclude $HOME/.gitexclude
 
+ln -sfnv $DIRNAME/.editorconfig $HOME/.editorconfig
 ln -sfnv $DIRNAME/.gacprc $HOME/.gacprc
+
+# claude code
+if [ ! -d $HOME/.claude ]; then
+  mkdir $HOME/.claude
+fi
+
+ln -sfnv $DIRNAME/.claude/CLAUDE.md $HOME/.claude/CLAUDE.md
+ln -sfnv $DIRNAME/.claude/setting.json $HOME/.claude/setting.json
