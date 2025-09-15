9459c3589661db877bba00600add26c7dd8a0924
Daisuke KONISHI <skydai1151@gmail.com>
2025-09-15 10:24:42 +0900

    feat: keychain設定とclaudeのalias追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,11 @@ end
 
 set pure_color_primary (set_color "7bb6db")
 
+
+alias claude="/home/dsk52/.claude/local/claude"
+
+# keychain設定
+if status is-interactive; and test -f /etc/os-release; and grep -q "Ubuntu" /etc/os-release
+    /usr/bin/keychain -q --nogui $HOME/.ssh/github/id_ed25519
+    source $HOME/.keychain/(hostname)-fish
+end
